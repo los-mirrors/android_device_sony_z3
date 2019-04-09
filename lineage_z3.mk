06d498fbba555c63d215baf0cc92d81c007e6138
moonlightly <906268147@qq.com>
2019-04-09 14:44:19 +0800

    update

---
+++ lineage_z3.mk
@@ -0,0 +1,16 @@
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, device/sony/z3/full_z3.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6603
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="leo-user N NPD56N 1000106 release-keys"
+
+BUILD_FINGERPRINT := Sony/leo/leo:N/NPD56N/1000106:user/release-keys
+
+PRODUCT_NAME := lineage_z3
+PRODUCT_DEVICE := z3
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="Myself5"
+
